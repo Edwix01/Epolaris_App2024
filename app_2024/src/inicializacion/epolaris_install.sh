72eedf7d0186c2bbb70cac70ead85e5405a1d129
Edwin Lima <115038833+Edwix01@users.noreply.github.com>
2024-05-31 19:17:19 -0500

    Update epolaris_install.sh

--- app_2024/src/inicializacion/epolaris_install.sh
+++ app_2024/src/inicializacion/epolaris_install.sh
@@ -2,6 +2,6 @@
 
 echo "EMPEZANDO CONFIGURACION"
 cd /home/du/Prototipo_App2024/app_2024/Epops
-nohup python3 mon3.py > mi_log.log 2>&1 &
+nohup python3 seg_proact.py > mi_log.log 2>&1 &
 
 echo "CONFIGURACION EXITOSA"
