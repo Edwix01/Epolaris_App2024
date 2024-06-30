544f8a9cf6c60e59cfd671f78d4d355c7c8f9ec4
Paola Mora Pedroza <158530803+paolamorap@users.noreply.github.com>
2024-06-29 19:35:41 -0500

    Update app.js

--- app_2024/src/app.js
+++ app_2024/src/app.js
@@ -11,11 +11,13 @@ const loginRoutes = require('./routes/login');
 const multer = require('multer');
 const app = express();
 
+const currentDir = __dirname;
+
 const PUERTO = 443;
 
 https.createServer({
-    key: fs.readFileSync('/home/du/Prototipo_App2024/app_2024/http/key.pem'),
-    cert: fs.readFileSync('/home/du/Prototipo_App2024/app_2024/http/cert.pem'),
+    key: fs.readFileSync(path.join(currentDir, 'http/localhost.key')),
+    cert: fs.readFileSync(path.join(currentDir, 'http/localhost.crt'))
 }, app).listen(PUERTO, function(){
     console.log('Escuchando en el puerto 443');
 });
